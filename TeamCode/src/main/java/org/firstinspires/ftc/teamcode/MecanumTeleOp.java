461464051ce4c29d8599b346d700c0420776ee22
zichenc7 <zichen.zc.cai@gmail.com>
2023-10-18 16:11:55 -0700

    updated imu settings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -102,7 +102,7 @@ public class MecanumTeleOp extends LinearOpMode {
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
             // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
+            if (gamepad1.y) {
                 imu.resetYaw();
             }
 
