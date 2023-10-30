43e96adf7f6b020fe61fc6503c5cb6b8cdccbbf8
digcheese <jeffxue269@gmail.com>
2023-10-30 16:42:28 -0700

    implemented drone launch abstracted some methods into a new class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,84 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class MecanumTeleOp extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeftMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backRightMotor;
-    // private DcMotor armMotor;
-    // private Servo droneLaunchServo;
-
-    public IMU setIMU() {
-        // Retrieve the IMU from the hardware map
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        boolean imuStatus = imu.initialize(parameters);
-        telemetry.addData("IMU", "Initialized: " + imuStatus);
-        telemetry.update();
-        return imu;
-    }
-
-    public void motorOp(IMU imu, double y, double x, double rx) {
-        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-        // Rotate the movement direction counter to the bot's rotation
-        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-        rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        double frontLeftPower = (rotY + rotX + rx) / denominator;
-        double backLeftPower = (rotY - rotX + rx) / denominator;
-        double frontRightPower = (rotY - rotX - rx) / denominator;
-        double backRightPower = (rotY + rotX - rx) / denominator;
-
-        frontLeftMotor.setPower(frontLeftPower);
-        backLeftMotor.setPower(backLeftPower);
-        frontRightMotor.setPower(frontRightPower);
-        backRightMotor.setPower(backRightPower);
-
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "frontLeft (%.2f)\n backLeft (%.2f)\n frontRight (%.2f)\n backRight (%.2f)", frontLeftPower, backLeftPower, frontRightPower, backRightPower);
-    }
-
-/*
+public class MecanumTeleOp extends OpModeBase {
+    public ElapsedTime runtime = new ElapsedTime();
+    /*
     public void armOp(double armUp, double armDown) {
         // armPower for future tuning
         // this has not been tested
         double armPower = armUp - armDown;
         armMotor.setPower(armPower);
         telemetry.addData("Arm", "power: (%.2f", armPower);
-    }
-*/
-/*
-    public void launchDrone {
-        // the position ranges from [0,1]
-        // 0 is 0 degrees, 0.5 is 90, 1 is 180
-        // double check to see if this is true for the servo we're using
-        // modify position when needed
-        double position = 0;
-        droneLaunchServo.setPosition(position);
     }
 */
     @Override
@@ -88,18 +28,24 @@ public class MecanumTeleOp extends LinearOpMode {
         telemetry.update();
 
         // motor initialization
-        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
         // armMotor = hardwareMap.dcMotor.get("armMotor");
-        // droneLaunchServo = hardwareMap.servo.get("droneLaunchServo");
+        Servo droneLaunchServo = hardwareMap.servo.get("droneLaunchServo");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        setFrontLeftMotor(frontLeftMotor);
+        setBackLeftMotor(backLeftMotor);
+        setFrontRightMotor(frontRightMotor);
+        setBackRightMotor(backRightMotor);
+        setDroneLaunchServo(droneLaunchServo);
 
         // Retrieve the IMU from the hardware map
         IMU imu = setIMU();
@@ -114,6 +60,7 @@ public class MecanumTeleOp extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
             // double armUp = gamepad1.left_trigger;
+
             // double armDown = -gamepad1.right_trigger;
 
             // This button choice was made so that it is hard to hit on accident,
@@ -121,21 +68,22 @@ public class MecanumTeleOp extends LinearOpMode {
             // The equivalent button is start on Xbox-style controllers.
             if (gamepad1.y) {
                 imu.resetYaw();
-                telemetry.addData("Status", "IMU resetted");
+                telemetry.addData("Status", "IMU reset");
                 telemetry.update();
             }
 
-            /*
-            if (gamepad1.start) {
-               //launchDrone();
-               telemetry.addData("Status", "Drone launched, hopefully");
+
+            if (gamepad1.x) {
+               launchDrone();
+               telemetry.addData("Status", "Drone launched");
                telemetry.update();
             }
-            */
 
-            motorOp(imu, y, x, rx);
+
+            double[] motors = motorOp(imu, y, x, rx);
+            telemetry.addData("Motors", "frontLeft (%.2f)\n backLeft (%.2f)\n frontRight (%.2f)\n backRight (%.2f)", motors[0], motors[1], motors[2], motors[3]);
             // armOp(armUp, armDown);
-
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeBase.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public abstract class OpModeBase extends LinearOpMode {
+    // constants
+    private static final double launchPos = 0;
+    private static final double restPos = 0.5;
+    // fields
+    private DcMotor frontLeftMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backRightMotor;
+    // private DcMotor armMotor;
+    private Servo droneLaunchServo;
+
+    public double[] motorOp(IMU imu, double y, double x, double rx) {
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+
+        return new double[]{frontLeftPower, backLeftPower, frontRightPower, backRightPower};
+    }
+
+    public IMU setIMU() {
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        boolean imuStatus = imu.initialize(parameters);
+        telemetry.addData("IMU", "Initialized: ", imuStatus);
+        return imu;
+    }
+
+    public void launchDrone() {
+        // the position ranges from [0,1]
+        // 0 is 0 degrees, 0.5 is 90, 1 is 180
+        // double check to see if this is true for the servo we're using
+        // modify position when needed
+        droneLaunchServo.setPosition(OpModeBase.launchPos);
+        sleep(500);
+        droneLaunchServo.setPosition(OpModeBase.restPos);
+    }
+
+    public void setBackLeftMotor(DcMotor backLeftMotor) {
+        this.backLeftMotor = backLeftMotor;
+    }
+    public DcMotor getBackLeftMotor() {
+        return backLeftMotor;
+    }
+    public void setBackRightMotor(DcMotor backRightMotor) {
+        this.backRightMotor = backRightMotor;
+    }
+    public DcMotor getBackRightMotor() {
+        return backRightMotor;
+    }
+    public void setFrontLeftMotor(DcMotor frontLeftMotor) {
+        this.frontLeftMotor = frontLeftMotor;
+    }
+    public DcMotor getFrontLeftMotor() {
+        return frontLeftMotor;
+    }
+    public void setFrontRightMotor(DcMotor frontRightMotor) {
+        this.frontRightMotor = frontRightMotor;
+    }
+    public DcMotor getFrontRightMotor() {
+        return frontRightMotor;
+    }
+    public void setDroneLaunchServo(Servo droneLaunchServo) {
+        this.droneLaunchServo = droneLaunchServo;
+    }
+    public Servo getDroneLaunchServo() {
+        return droneLaunchServo;
+    }
+}
